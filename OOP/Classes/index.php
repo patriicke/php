d8b0a2e34b7213f65feae73c8e15e5dad1add60d
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-17 05:42:49 +0200

    This is the Object Oriented Programming in PHP

---
+++ OOP/Classes/index.php
@@ -0,0 +1,30 @@
+<?php
+    class  Person{
+    protected $goal = "Goals";
+    
+    //    function __construct($fname,$lname,$age){
+    //         $this->fname=$fname;
+    //         $this->lname=$lname;
+    //         $this->age=$age;
+    //     }
+    //     private function getLname(){
+    //         return $this->fname;
+    //     }
+    }
+    class Goal extends Person{
+       protected $goal = "Goooooal";
+        // public function setGoal($newData){
+        //     return $this->goal= $newData;
+        // }
+public function showGoal(){
+            return $this->goal;
+        }
+        
+    }
+    $person1 = new Goal();
+    // $person1->setGoal("Not a goal");
+    echo $person1->showGoal();
+    
+  
+    
+?>
