701bd8297b3b2e684b8eb87756ef84cb5680adc7
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-27 11:31:35 +0200

    I have modified my own function to add an item in php without using another function

--- NAMESPACES/index.php
+++ NAMESPACES/index.php
@@ -1,22 +1,33 @@
 <?php
-class SimpleClass
-{
-    // property declaration
-    public $var = 'a default value';
-    // method declaration
-    public function displayVar()
-    {
-        echo $this->var;
-    }
-}
-class ExtendClass extends SimpleClass
-{
-    // Redefine the parent method
-    function displayVar()
-    {
-        echo "Extending class<br/>";
-        parent::displayVar();
-    }
-}
-$extended = new ExtendClass();
-$extended->displayVar();
+// abstract class SimpleClass
+// {
+//     // property declaration
+//     public $var = 'a default value';
+//     // method declaration
+//     public function displayVar()
+//     {
+//         echo $this->var;
+//     }
+// }
+// class ExtendClass extends SimpleClass
+// {
+//     // Redefine the parent method
+//     function displayVar()
+//     {
+//         echo "Extending class<br/>";
+//         parent::displayVar();
+//     }
+// }
+// $extended = new ExtendClass();
+// $extended->displayVar();
+
+// define("SCHOOL", "RWANDA CODING ACADEMY",true);
+// echo school;
+// echo "<br>";
+const SCHOOL = "RWANDA CODING ACADEMY";
+echo SCHOOL;
+echo "<br>";
+const SCHOOL = "RCA";
+echo SCHOOL;
+// const SCHOOL = "RCA";
+// echo $SCHOOL;
